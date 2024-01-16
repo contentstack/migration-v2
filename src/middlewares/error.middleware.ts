8fca3ed2fd5842e61c4e3110e643cb3dd9fe4a19
hanoak20 <hanoak.suchethan@contentstack.com>
2024-01-16 14:11:41 +0530

    fix: added back the 'next' method to the error middleware.

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,7 +1,13 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { AppError } from "../utils/custom-errors.utils";
 
-export const errorMiddleware = (err: Error, req: Request, res: Response) => {
+export const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
   if (err instanceof AppError) {
     res
       .status(err.statusCode)
