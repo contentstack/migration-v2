ded91e740ac3f3f9586bd4e5daa8822b66dfe95f
hanoak20 <hanoak.suchethan@contentstack.com>
2024-01-17 15:12:26 +0530

    feat: made server.ts simple and added middlewares intsead.

---
+++ src/middlewares/req-headers.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+export const requestHeadersMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept, app_token"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+};
