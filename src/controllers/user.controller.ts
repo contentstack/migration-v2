0dca071692babfd9db19d52d4a5062ae9d73925f
hanoak20 <hanoak.suchethan@contentstack.com>
2024-01-19 18:32:23 +0530

    chore: DB query logic added the the user's service.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { userService } from "../services/user.service";
+import { constants } from "../constants";
 
 const getUserProfile = async (req: Request, res: Response) => {
   const user = await userService.getUserProfile(req);
-  res.status(200).json(user);
+  res.status(constants.HTTP_CODES.OK).json(user);
 };
 
 export const userController = {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,49 +1,50 @@
 import { Request } from "express";
 import { config } from "../config";
 import https from "../utils/https.utils";
-import * as fs from "fs/promises";
-import { LoginServiceType, UserProfile } from "../models/types";
+import { AppTokenPayload, UserProfile } from "../models/types";
 import { constants } from "../constants";
 import {
   BadRequestError,
   InternalServerError,
 } from "../utils/custom-errors.utils";
+import AuthenticationModel from "../models/authentication";
 
-const getUserProfile = async (
-  req: Request
-): Promise<UserProfile | LoginServiceType> => {
+const getUserProfile = async (req: Request): Promise<UserProfile> => {
   try {
-    //TODO: replace the current logic with the actual db-fetch logic
-    const tokens = JSON.parse(await fs.readFile(`tokens.json`, "utf8"));
-    const authtoken = tokens?.[req?.headers?.app_token as string];
+    const appTokenPayload: AppTokenPayload = req?.body?.token_payload;
 
-    const apiResponse =
-      authtoken &&
-      (await https({
-        method: "GET",
-        url: `${config.CS_API.US}/user?include_orgs_roles=true`,
-        headers: {
-          "Content-Type": "application/json",
-          authtoken: authtoken,
-        },
-      }));
+    const user = await AuthenticationModel.findOne({
+      user_id: appTokenPayload?.user_id,
+      region: appTokenPayload.region,
+    }).lean();
 
-    if (!apiResponse?.data?.user)
+    if (!user?.authtoken)
       throw new BadRequestError(constants.HTTP_TEXTS.NO_CS_USER);
 
-    const orgs = apiResponse?.data?.user?.organizations
+    const res = await https({
+      method: "GET",
+      url: `${config.CS_API.US}/user?include_orgs_roles=true`,
+      headers: {
+        "Content-Type": "application/json",
+        authtoken: user?.authtoken,
+      },
+    });
+
+    if (!res?.data?.user)
+      throw new BadRequestError(constants.HTTP_TEXTS.NO_CS_USER);
+
+    const orgs = (res?.data?.user?.organizations || [])
       ?.filter((org: any) => org?.org_roles?.some((item: any) => item.admin))
       ?.map(({ uid, name }: any) => ({ org_id: uid, org_name: name }));
 
-    const userProfile: UserProfile = {
+    return {
       user: {
-        email: apiResponse?.data?.user?.email,
-        first_name: apiResponse?.data?.user?.first_name,
-        last_name: apiResponse?.data?.user?.last_name,
+        email: res?.data?.user?.email,
+        first_name: res?.data?.user?.first_name,
+        last_name: res?.data?.user?.last_name,
         orgs: orgs,
       },
     };
-    return userProfile;
   } catch (error) {
     throw new InternalServerError("Error while getting user profile");
   }
