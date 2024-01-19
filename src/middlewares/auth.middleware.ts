d582ac2c324b72f1b7f0b10244ccf56995c2d1ba
hanoak20 <hanoak.suchethan@contentstack.com>
2024-01-19 16:15:01 +0530

    chore: updated the authenticateUser middleware to attach app_token's payload

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,28 +2,30 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { config } from "../config";
+import { constants } from "../constants";
 
 export const authenticateUser = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  // authentication logic (check for a valid token)
+  const status = constants.HTTP_CODES.UNAUTHORIZED;
   const token = req.get("app_token");
-  if (token) {
-    jwt.verify(token, config.APP_TOKEN_KEY, (err, decoded) => {
-      if (err) {
-        return res
-          .status(401)
-          .json({ message: "Unauthorized - Invalid token" });
-      }
 
-      // Attach the decoded token to the request object for later use
-      (req as any).decodedToken = decoded;
+  if (!token)
+    return res
+      .status(status)
+      .json({ status, message: "Unauthorized - Token missing" });
 
-      next();
-    });
-  } else {
-    return res.status(401).json({ message: "Unauthorized - Token missing" });
-  }
+  jwt.verify(token, config.APP_TOKEN_KEY, (err, payload) => {
+    if (err)
+      return res
+        .status(status)
+        .json({ status, message: "Unauthorized - Invalid token" });
+
+    // Attach the payload to the request object for later use
+    (req as any).body.token_payload = payload;
+
+    next();
+  });
 };
