a7bcb23a51579d03214d50d8f0860ffd11481456
hanoak20 <hanoak.suchethan@contentstack.com>
2024-03-13 13:21:26 +0530

    fix: removed winston logger's typescript error.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,13 +5,9 @@ const logger = createLogger({
   level: "info",
   format: format.combine(format.timestamp(), format.json()),
   transports: [
-    // - Write all logs with importance level of `error` or less to `error.log`
-    new transports.File({ filename: "error.log", level: "error" }),
     // - Write all logs with importance level of `info` or less to `combined.log`
     new transports.File({ filename: "combine.log" }),
-    new transports.Console({
-      format: format.combine(format.timestamp(), format.prettyPrint()),
-    }),
+    new transports.Console({}),
   ],
 });
 
