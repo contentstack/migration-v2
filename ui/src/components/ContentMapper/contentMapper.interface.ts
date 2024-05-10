ac8966226977c97bc655541f3e69deca06160cf3
Sayali Joshi <sayali.joshi@contentstack.com>
2024-05-10 17:46:56 +0530

    [CMG-102] - UI for for On row uncheck make isdeleted value to true

--- ui/src/components/ContentMapper/contentMapper.interface.ts
+++ ui/src/components/ContentMapper/contentMapper.interface.ts
@@ -100,3 +100,11 @@ export interface optionsType {
 export interface ExstingContentTypeMatch {
   [key: string]: string;
 }
+
+export interface UidMap {
+  [key: string]: boolean;
+}
+
+export interface ContentTypeMap {
+  [key: string]: string;
+}
