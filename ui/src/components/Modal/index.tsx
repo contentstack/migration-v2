d53d0224f1b32739274b2a5177b757df06ebfc91
Sayali Joshi <sayali.joshi@contentstack.com>
2024-05-14 17:35:51 +0530

    [CMG-146], [CMG-149], [CMG-150]

--- ui/src/components/Modal/index.tsx
+++ ui/src/components/Modal/index.tsx
@@ -47,16 +47,14 @@ const Modal = (props: ProjectModalProps) => {
   };
 
   const nameValidation = (value: string) => {
-
-    if (value === '') {
+    if (!value || !/^[^\s].+[^\s]$/.test(value)) {
       setInputValue(false);
-      return 'Please enter project name.';
-    } else if (value && value?.length > 200) {
+      return 'Please enter project name.'
+    } else if(value && value?.length > 200) {
       setInputValue(false);
       return 'Project Name should not be more than 200 chars';
     } else {
       setInputValue(true);
-      return '';
     }
   };
 
@@ -68,6 +66,7 @@ const Modal = (props: ProjectModalProps) => {
       return '';
     }
   };
+
   return (
     <>
       <ModalHeader
