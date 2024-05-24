5806219ad0a4eb4647df210cd2520e1178787743
Sayali Joshi <sayali.joshi@contentstack.com>
2024-05-24 20:30:42 +0530

    Mapping corrections on the fields

--- ui/src/components/AdvancePropertise/index.tsx
+++ ui/src/components/AdvancePropertise/index.tsx
@@ -6,7 +6,9 @@ import {
   FieldLabel,
   TextInput,
   ToggleSwitch,
-  Tooltip
+  Tooltip,
+  Radio,
+  Info
 } from '@contentstack/venus-components';
 
 import './index.scss';
@@ -57,63 +59,113 @@ const AdvancePropertise = (props: SchemaProps) => {
 
   return (
     <>
-      <ModalHeader title={`${props?.fieldtype} propertise`} closeModal={props?.closeModal} />
+      <ModalHeader title={`${props?.fieldtype} propertise`} closeModal={props?.closeModal} className="text-capitalize" />
       <ModalBody>
-        <FieldLabel htmlFor="someInput" version="v2">
-          Validation (Regex)
-        </FieldLabel>
-        <TextInput
-          className="validation-input"
-          type="text"
-          value={toggleStates?.validationRegex}
-          placeholder="Enter value"
-          version="v2"
-          onChange={(e: any) => handleOnChange('validationRegex', e, true)}
-        />
-        <FieldLabel className="option-label" htmlFor="someInput" version="v2">
-          Options
-        </FieldLabel>
-        <div className="options-class">
-          <ToggleSwitch
-            label="Mandatory"
-            labelColor="primary"
-            labelPosition="right"
-            checked={toggleStates?.mandatory}
-            onChange={(e: any) => handleToggleChange('mandatory', e?.target?.checked, true)}
-          />
-          <ToggleSwitch
-            label="Multiple"
-            labelColor="primary"
-            labelPosition="right"
-            checked={toggleStates?.multiple}
-            onChange={(e: any) => handleToggleChange('multiple', e?.target?.checked, true)}
-          />
-          <ToggleSwitch
-            label="Unique"
-            labelColor="primary"
-            labelPosition="right"
-            checked={toggleStates?.unique}
-            onChange={(e: any) => handleToggleChange('unique', e?.target?.checked, true)}
-          />
-          <Tooltip
-            content="Available only if there are multiple languages in your stack"
-            position="top"
-            disabled={props?.isLocalised}
-          >
-            <ToggleSwitch
-              id="disabled"
-              disabled={!props?.isLocalised}
-              label="Non-localizable"
-              labelColor="primary"
-              labelPosition="right"
-              checked={toggleStates?.nonLocalizable}
-              onChange={(e: any) => handleToggleChange('nonLocalizable', e?.target?.checked, true)}
-            />
-          </Tooltip>
-          <p className="non-localizable-message">
-            If enabled, editing this field is restricted in localized entries. The field will use
-            the value of the master-language entry in all localized entries.
-          </p>
+        <div className='modal-data'>
+          {props?.fieldtype === 'HTML Rich text Editor' && (
+            <div>
+              <FieldLabel htmlFor="editorType" version="v2" className='radio-field'>Editor Type</FieldLabel>
+              <div
+                className="Radio-wrapper FormFields__selection-wrapper--inline"
+              >
+                <Radio
+                  checked="true"
+                  id="basic"
+                  label="Basic"
+                  name="option"
+                  // onChange={function noRefCheck(){}}
+                  // onClick={function noRefCheck(){}}
+                  radioDisplayType="inline-flex"
+                />
+                <Radio
+                  id="advanced"
+                  label="Advanced"
+                  name="option"
+                  // onChange={function noRefCheck(){}}
+                  // onClick={function noRefCheck(){}}
+                  radioDisplayType="inline-flex"
+                />
+                <Radio
+                  id="custom"
+                  label="Custom"
+                  name="option"
+                  // onChange={function noRefCheck(){}}
+                  // onClick={function noRefCheck(){}}
+                  radioDisplayType="inline-flex"
+                />
+              </div>
+
+              <Info
+                content="Provides basic formatting tools as well as options to insert images, videos, tables, links, and custom class or ID."
+                className='info-style'
+                // type="default"
+                version="v2"
+                backgroundColor='#f5fdff'
+                // borderColor='#0469e3'
+              />
+            </div>
+          )}
+
+          {props?.fieldtype === 'text' || props?.fieldtype === 'Single Line Textbox' && (
+            <div>
+              <FieldLabel htmlFor="someInput" version="v2">
+                Validation (Regex)
+              </FieldLabel>
+              <TextInput
+                className="validation-input"
+                type="text"
+                value={toggleStates?.validationRegex}
+                placeholder="Enter value"
+                version="v2"
+                onChange={(e: any) => handleOnChange('validationRegex', e, true)}
+              />
+              <FieldLabel className="option-label" htmlFor="someInput" version="v2">
+                Options
+              </FieldLabel>
+              <div className="options-class">
+                <ToggleSwitch
+                  label="Mandatory"
+                  labelColor="primary"
+                  labelPosition="right"
+                  checked={toggleStates?.mandatory}
+                  onChange={(e: any) => handleToggleChange('mandatory', e?.target?.checked, true)}
+                />
+                <ToggleSwitch
+                  label="Multiple"
+                  labelColor="primary"
+                  labelPosition="right"
+                  checked={toggleStates?.multiple}
+                  onChange={(e: any) => handleToggleChange('multiple', e?.target?.checked, true)}
+                />
+                <ToggleSwitch
+                  label="Unique"
+                  labelColor="primary"
+                  labelPosition="right"
+                  checked={toggleStates?.unique}
+                  onChange={(e: any) => handleToggleChange('unique', e?.target?.checked, true)}
+                />
+                <Tooltip
+                  content="Available only if there are multiple languages in your stack"
+                  position="top"
+                  disabled={props?.isLocalised}
+                >
+                  <ToggleSwitch
+                    id="disabled"
+                    disabled={!props?.isLocalised}
+                    label="Non-localizable"
+                    labelColor="primary"
+                    labelPosition="right"
+                    checked={toggleStates?.nonLocalizable}
+                    onChange={(e: any) => handleToggleChange('nonLocalizable', e?.target?.checked, true)}
+                  />
+                </Tooltip>
+                <p className="non-localizable-message">
+                  If enabled, editing this field is restricted in localized entries. The field will use
+                  the value of the master-language entry in all localized entries.
+                </p>
+              </div>
+            </div>
+          )}
         </div>
       </ModalBody>
     </>
