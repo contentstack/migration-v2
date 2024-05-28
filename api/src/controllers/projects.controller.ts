c6ba073f851835122f2820187fc022eb66ea5632
Shristi Sinha <shristi.sinha@contentstack.com>
2024-05-28 13:44:27 +0530

    apply validation

--- api/src/controllers/projects.controller.ts
+++ api/src/controllers/projects.controller.ts
@@ -61,7 +61,7 @@ const deleteProject = async (req: Request, res: Response): Promise<void> => {
 
 const revertProject = async (req: Request, res: Response): Promise<void> => {
   const project = await projectService.revertProject(req);
-  res.status(200).json(project);
+  res.status(project.status).json(project);
 };
 
 export const projectController = {
