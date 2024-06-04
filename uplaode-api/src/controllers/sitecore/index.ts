62bd42b2ba9909d7d7512c132ae93468cc632373
umeshmore45 <umesh.more@contentstack.com>
2024-06-04 16:30:42 +0530

    added mapper

--- uplaode-api/src/controllers/sitecore/index.ts
+++ uplaode-api/src/controllers/sitecore/index.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { readFileSync } from "fs";
+import { deleteFolderSync } from "../../helper";
+import logger from "../../utils/logger";
+import { HTTP_CODES, HTTP_TEXTS } from "../../constants";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { contentTypes, ExtractConfiguration, reference } = require('migration-sitecore');
+const { contentTypes, ExtractConfiguration, reference, ExtractFiles } = require('migration-sitecore');
 
-const createSitecoreMapper = async () => {
+const createSitecoreMapper = async (filePath: string = "", projectId: string | string[], app_token: string | string[]) => {
   try {
-    const path = "/Users/umesh.more/Downloads/package 45/items";
+    const path = `${filePath}/items`;
+    await ExtractFiles(path);
     await ExtractConfiguration(path);
     await contentTypes(path);
     const infoMap = await reference();
@@ -17,22 +21,31 @@ const createSitecoreMapper = async () => {
           JSON.parse(readFileSync(`${infoMap?.path}/content_types/${contentType}`, 'utf8'))
         );
       }
-      console.log("🚀 ~ createSitecoreMapper ~ fieldMapping:", fieldMapping)
       const config = {
         method: 'post',
         maxBodyLength: Infinity,
-        url: `http://localhost:5000/v2/mapper/createDummyData/98ee1edc-f297-419d-a394-8ab71e2e546c`,
+        url: `${process.env.NODE_BACKEND_API}/v2/mapper/createDummyData/${projectId}`,
         headers: {
-          'app_token': '***REMOVED***',
+          app_token,
           'Content-Type': 'application/json'
         },
         data: JSON.stringify(fieldMapping),
       };
       const response = await axios.request(config)
-      console.log("🚀 ~ forawait ~ response:", response?.data)
+      if (response?.data?.content_mapper?.length) {
+        deleteFolderSync(infoMap?.path);
+        logger.info('Validation success:', {
+          status: HTTP_CODES?.OK,
+          message: HTTP_TEXTS?.MAPPER_SAVED,
+        });
+      }
     }
   } catch (err: any) {
     console.error("🚀 ~ createSitecoreMapper ~ err:", err?.response?.data ?? err)
+    logger.warn('Validation error:', {
+      status: HTTP_CODES?.UNAUTHORIZED,
+      message: HTTP_TEXTS?.VALIDATION_ERROR,
+    });
   }
 }
 
