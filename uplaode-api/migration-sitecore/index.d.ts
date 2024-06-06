57eb3ca714598669a8eb46f19a8620a7b269eba2
umeshmore45 <umesh.more@contentstack.com>
2024-06-06 15:59:16 +0530

    resloved conflict

--- uplaode-api/migration-sitecore/index.d.ts
+++ uplaode-api/migration-sitecore/index.d.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 import contentTypes from "./libs/contenttypes.js";
 
-// module.exports = { contentTypes = contentTypes?.ExtractContentTypes };
 export default contentTypes;
