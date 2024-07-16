561562b8912b8dd0479b838284017bf32bb44142
Rohit Kini <rohit.kini@contentstack.com>
2024-07-16 15:39:32 +0530

    added comments on api controllers,middlewares,models

--- api/src/middlewares/logger.middleware.ts
+++ api/src/middlewares/logger.middleware.ts
@@ -2,6 +2,16 @@ import expressWinston from "express-winston";
 import logger from "../utils/logger.js";
 
 //Logger Middleware to log every request
+/**
+ * Express middleware for logging HTTP requests.
+ *
+ * @remarks
+ * This middleware uses `express-winston` to log HTTP requests with the specified options.
+ *
+ * @param req - The Express request object.
+ * @param res - The Express response object.
+ * @param next - The next middleware function.
+ */
 const loggerMiddleware = expressWinston.logger({
   level: "info",
   colorize: true,
