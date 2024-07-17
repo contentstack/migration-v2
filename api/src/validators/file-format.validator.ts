b269854a0c02920d8865054cfecc783442fce4c9
Rohit Kini <rohit.kini@contentstack.com>
2024-07-17 12:18:00 +0530

    added comments on validators

--- api/src/validators/file-format.validator.ts
+++ api/src/validators/file-format.validator.ts
@@ -1,6 +1,11 @@
 import { checkSchema } from "express-validator";
 import { VALIDATION_ERRORS } from "../constants/index.js";
 
+/**
+ * Validates the file format.
+ *
+ * @returns {Object} The validation schema for the file format.
+ */
 export default checkSchema({
   file_format: {
     in: "body",
