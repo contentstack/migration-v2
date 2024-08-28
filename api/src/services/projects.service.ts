8fcfd10a8347bb5fd1a087bbf6fb4b2409681faa
RohitKini <rohit.kini@contentstack.com>
2024-08-28 15:20:31 +0530

    added stack details in project db

--- api/src/services/projects.service.ts
+++ api/src/services/projects.service.ts
@@ -119,6 +119,7 @@ const createProject = async (req: Request) => {
     isDeleted: false,
     isNewStack: false,
     newStackId: "",
+    stackDetails: [],
   };
 
   try {
@@ -206,6 +207,7 @@ const updateProject = async (req: Request) => {
       }
       data.projects[projectIndex].updated_by = user_id;
       data.projects[projectIndex].updated_at = new Date().toISOString();
+      data.projects[projectIndex].stackDetails = updateData?.stackDetails;
       project = data.projects[projectIndex];
     });
 
@@ -911,6 +913,67 @@ const revertProject = async (req: Request) => {
   }
 };
 
+/**
+ * Updates the stack details for a project.
+ *
+ * @param req - The request object containing the parameters and body.
+ * @returns An object with the status and data of the update operation.
+ * @throws BadRequestError if the project status is invalid.
+ * @throws ExceptionFunction if an error occurs during the update.
+ */
+const updateStackDetails = async (req: Request) => {
+  const { orgId, projectId } = req.params;
+  const { token_payload, stack_details } = req.body;
+  const srcFunc = "updateStackDetails";
+
+  await ProjectModelLowdb.read();
+  const projectIndex = (await getProjectUtil(
+    projectId,
+    {
+      id: projectId,
+      org_id: orgId,
+      region: token_payload?.region,
+      owner: token_payload?.user_id,
+    },
+    srcFunc,
+    true
+  )) as number;
+
+  try {
+    ProjectModelLowdb.update((data: any) => {
+      data.projects[projectIndex].stackDetails = stack_details;
+      data.projects[projectIndex].updated_at = new Date().toISOString();
+    });
+
+    logger.info(
+      getLogMessage(
+        srcFunc,
+        `Stack Details for project [Id : ${projectId}] has been successfully updated.`,
+        token_payload
+      )
+    );
+    return {
+      status: HTTP_CODES.OK,
+      data: {
+        message: HTTP_TEXTS.CMS_UPDATED,
+      },
+    };
+  } catch (error: any) {
+    logger.error(
+      getLogMessage(
+        srcFunc,
+        `Error occurred while updating stack details for project [Id : ${projectId}].`,
+        token_payload,
+        error
+      )
+    );
+    throw new ExceptionFunction(
+      error?.message || HTTP_TEXTS.INTERNAL_ERROR,
+      error?.statusCode || error?.status || HTTP_CODES.SERVER_ERROR
+    );
+  }
+};
+
 export const projectService = {
   getAllProjects,
   getProject,
@@ -925,4 +988,5 @@ export const projectService = {
   updateCurrentStep,
   deleteProject,
   revertProject,
+  updateStackDetails,
 };
