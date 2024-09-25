e08cfeb1a1086412fdd2911e5e2dfbf4e7553543
Arpit1119 <arpit.tyagi@contentstack.com>
2024-09-25 15:21:42 +0530

    fix: updated createMapper service

--- upload-api/src/services/createMapper.ts
+++ upload-api/src/services/createMapper.ts
@@ -2,15 +2,15 @@ import createSitecoreMapper from '../controllers/sitecore';
 import { Config } from '../models/types';
 import createContentfulMapper from './contentful';
 
-const createMapper = (filePath: string = "", projectId: string | string[], app_token: string | string[], affix: string | string[], config: Config) => {
+const createMapper = async (filePath: string = "", projectId: string | string[], app_token: string | string[], affix: string | string[], config: Config) => {
   const CMSIdentifier = config?.cmsType?.toLowerCase();
   switch (CMSIdentifier) {
     case 'sitecore': {
-      return createSitecoreMapper(filePath, projectId, app_token, affix, config);
+      return await createSitecoreMapper(filePath, projectId, app_token, affix, config);
     }
 
     case 'contentful': {
-      return createContentfulMapper(projectId, app_token, affix, config);
+      return await createContentfulMapper(projectId, app_token, affix, config);
     }
 
     // case 'wordpress': {
