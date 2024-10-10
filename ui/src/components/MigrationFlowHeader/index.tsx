0580583967affb3869e3ce6e362733ad7d8cd9cd
Sayali Joshi <sayali.joshi@contentstack.com>
2024-10-10 13:31:01 +0530

    Update current step api changes

--- ui/src/components/MigrationFlowHeader/index.tsx
+++ ui/src/components/MigrationFlowHeader/index.tsx
@@ -67,7 +67,7 @@ const MigrationFlowHeader = ({projectData, handleOnClick, isLoading }: Migration
         version="v2"
         aria-label='Save and Continue'
         isLoading={isLoading}
-        disabled={newMigrationData?.testStacks?.some((stack) => stack?.isMigrated === false)}
+        disabled={params?.stepId === '4' && !newMigrationData?.test_migration?.isMigrationComplete}
       >
         {stepValue}
       </Button>
