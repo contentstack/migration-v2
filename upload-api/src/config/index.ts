f9b668a07e8958a6344dbf342ff08cf2f72102d8
GaurishN <gaurish.naik@raweng.com>
2024-10-17 12:44:20 +0530

    added filepath constants and removed hardcoded values

--- upload-api/src/config/index.ts
+++ upload-api/src/config/index.ts
@@ -12,5 +12,5 @@ export default {
     bucketName: 'migartion-test',
     buketKey: 'project/package 45.zip'
   },
-  localPath: '/home/gaurishn/Documents/contentstack/package 45.zip'
+  localPath: '/Users/umesh.more/Documents/ui-migration/migration-v2-node-server/upload-api/extracted_files/package 45.zip'
 };
