ec678593b53ffde74a529acdeb77ce07c3cfce76
GaurishN <gaurish.naik@raweng.com>
2024-10-17 12:37:23 +0530

    changed hardcoded filepath values and changes ContentstackFieldType to contentstackFieldType

--- ui/src/pages/Migration/index.tsx
+++ ui/src/pages/Migration/index.tsx
@@ -50,7 +50,6 @@ const Migration = () => {
   const [curreentStepIndex, setCurrentStepIndex] = useState(0);
   const [isCompleted, setIsCompleted] = useState<boolean>(false);
   const [isProjectMapper, setIsProjectMapper] = useState<boolean>(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const params: Params<string> = useParams();
   const { projectId = '' } = useParams();
@@ -80,19 +79,33 @@ const Migration = () => {
 
    // Function to get exisiting content types list
    const fetchExistingContentTypes = async () => {
-    const { data, status } = await getExistingContentTypes(projectId);
-    if (status === 201) {
-      return data?.contentTypes;
+    try {
+      const { data, status } = await getExistingContentTypes(projectId);
+      if (status === 201) {
+        return data?.contentTypes;
+      }
+      
+    } catch (error) {
+      return error;
+      
     }
+    
   };
 
   // Function to get exisiting global fields list
   const fetchExistingGlobalFields = async () => {
-    const { data, status } = await getExistingGlobalFields(projectId);
+    try {
+      const { data, status } = await getExistingGlobalFields(projectId);
 
-    if (status === 201) {
-      return data?.globalFields;
+      if (status === 201) {
+        return data?.globalFields;
+      }
+      
+    } catch (error) {
+      return error;
+      
     }
+   
   }
 
   const fetchData = async () => {
@@ -166,6 +179,9 @@ const Migration = () => {
     created_at: '',
     isNewStack: false
   };
+
+  console.log('projectData', projectData);
+  
   
   selectedStackData = {
     label: projectData?.stackDetails?.label,
@@ -394,14 +410,14 @@ const Migration = () => {
   };
 
   const handleOnClickContentMapper = async (event: MouseEvent) => {
-    setIsModalOpen(true);
+    // setIsModalOpen(true);
 
     if(newMigrationData?.content_mapping?.isDropDownChanged) {
       return cbModal({
         component: (props: ModalObj) => (
         <SaveChangesModal
             {...props}
-            isopen={setIsModalOpen}
+            // isopen={setIsModalOpen}
             otherCmsTitle={newMigrationData?.content_mapping?.otherCmsTitle}
             saveContentType={saveRef?.current?.handleSaveContentType}
             changeStep={async () => {
