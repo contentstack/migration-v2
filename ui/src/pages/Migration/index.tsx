1691a0dea4e3604b26081ca8ec80525b96b98463
AishDani <aishwarya.dani@contentstack.com>
2024-12-08 16:58:11 +0530

    refactor:removed redux values of affix and validation from projectmapper

--- ui/src/pages/Migration/index.tsx
+++ ui/src/pages/Migration/index.tsx
@@ -224,7 +224,7 @@ const Migration = () => {
         ...newMigrationData?.legacy_cms,
         selectedCms: selectedCmsData,
         selectedFileFormat: selectedFileFormatData,
-        affix:  projectData?.legacy_cms?.affix ?? newMigrationData?.legacy_cms?.affix,
+        affix:  projectData?.legacy_cms?.affix ,
         uploadedFile: {
           file_details: {
             localPath: projectData?.legacy_cms?.file_path,
@@ -235,7 +235,7 @@ const Migration = () => {
             },
             isLocalPath: projectData?.legacy_cms?.is_localPath
           },
-          isValidated:  projectData?.legacy_cms?.is_fileValid || newMigrationData?.legacy_cms?.uploadedFile?.isValidated,
+          isValidated:  projectData?.legacy_cms?.is_fileValid ,
           reValidate: newMigrationData?.legacy_cms?.uploadedFile?.reValidate
         },
         isFileFormatCheckboxChecked: true, 
