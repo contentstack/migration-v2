31d313992fc7929504197e09dbb7c305f501582f
sauravraw <saurav.upadhyay@contentstack.com>
2025-02-11 18:47:33 +0530

    Window OS support code added

--- upload-api/src/config/index.ts
+++ upload-api/src/config/index.ts
@@ -1,17 +1,30 @@
 export default {
   plan: {
-    dropdown: { optionLimit: 100 }
+    dropdown: { optionLimit: 100 } // Defines the maximum number of dropdown options allowed. Default is 100.
   },
-  cmsType: 'Wordpress',
-  isLocalPath: true,
+
+  // CMS Type
+  cmsType: 'Wordpress', // Specifies the type of CMS being used. Default is 'Sitecore', but can be changed based on the project.
+
+  // Local Path Indicator
+  //   - If true, the migration files will be accessed from the local system.
+  //   - If false, AWS S3 will be used for migration files.
+  isLocalPath: true, // A boolean flag to determine if the provided path is local.
+
+  // Local Path Configuration
+  //   - Required when `isLocalPath` is true.
+  //   - Example: '/upload-api/extracted_files/package 45.zip'
+  localPath: '/home/gaurishn/Documents/contentstack/sitetitle.xml', // - The file path where extracted migration files are stored locally.
+
+  // AWS Configuration (Optional)
+  // - This is used only when `isLocalPath` is set to false
+  //   and no package path is provided inside `localPath`.
   awsData: {
-    awsRegion: 'us-east-2',
-    awsAccessKeyId: '',
-    awsSecretAccessKey: '',
-    awsSessionToken: '',
-    bucketName: 'migartion-test',
-    buketKey: 'project/package 45.zip'
-  },
-  localPath: '/home/gaurishn/Documents/contentstack/sitetitle.xml' //package 45.zip'
- // localPath: '/Users/umesh.more/Documents/ui-migration/migration-v2-node-server/upload-api/extracted_files/package 45.zip'
+    awsRegion: 'us-east-2', // Specifies the AWS region. Default is 'us-east-2'.
+    awsAccessKeyId: '', // The access key ID for AWS authentication.
+    awsSecretAccessKey: '', // The secret access key for AWS authentication.
+    awsSessionToken: '', // The session token used for temporary AWS authentication.
+    bucketName: 'migartion-test', // The name of the AWS S3 bucket used for storing migration files. Default is 'migration-test'.
+    bucketKey: 'project/package 45.zip' // The specific key (file path) within the S3 bucket. Default is 'project/package 45.zip'.
+  }
 };
