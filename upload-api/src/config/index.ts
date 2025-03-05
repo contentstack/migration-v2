7d61294346abf5cbdd9b365324a47a7b051f00ef
Sayali Joshi <sayali.joshi@contentstack.com>
2025-03-05 18:11:23 +0530

    [CMG-538] - Contentful v1.0.0 | Content mapper | For group field selection or deselection, child is not selecting or deselecting and with that in test stack, its migrating  Title and Url only

--- upload-api/src/config/index.ts
+++ upload-api/src/config/index.ts
@@ -2,7 +2,7 @@ export default {
   plan: {
     dropdown: { optionLimit: 100 }
   },
-  cmsType: 'Wordpress',
+  cmsType: 'contentful',
   isLocalPath: true,
   awsData: {
     awsRegion: 'us-east-2',
@@ -12,7 +12,6 @@ export default {
     bucketName: 'migartion-test',
     buketKey: 'project/package 45.zip'
   },
-  
-  localPath: '/home/gaurishn/Documents/contentstack/sitetitle.xml' //package 45.zip'
+  localPath: '/Users/sayali.joshi/Downloads/contentfulDummyEmbedData.json' //package 45.zip'
  // localPath: '/Users/umesh.more/Documents/ui-migration/migration-v2-node-server/upload-api/extracted_files/package 45.zip'
 };
