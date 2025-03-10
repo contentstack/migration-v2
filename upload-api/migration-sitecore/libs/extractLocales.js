63ae0a379f1a15d691bd2daf82c80a0322f233e2
AishDani <aishwarya.dani@contentstack.com>
2025-03-10 10:54:10 +0530

    refactor:conflict in upload siteocore service

--- upload-api/migration-sitecore/libs/extractLocales.js
+++ upload-api/migration-sitecore/libs/extractLocales.js
@@ -1,33 +1,32 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require("fs");
-const path = require("path");
-
+const fs = require('fs');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 
 const uniqueLanguages = new Set(); // Define uniqueLanguages globally or pass it as a parameter
 
-const extractLocales = (dir) => {
-    const items = fs.readdirSync(dir, { withFileTypes: true });
+const findAndExtractLanguages = (dir) => {
+  const items = fs.readdirSync(dir, { withFileTypes: true });
 
-    for (const item of items) {
-        const fullPath = path.join(dir, item.name);
-        
-        if (item.isDirectory()) {
-            extractLocales(fullPath); // Proper recursion
-        } else if (item.isFile() && item.name === "data.json.json") {
-            try {
-                const rawData = fs.readFileSync(fullPath, "utf8");
-                const jsonData = JSON.parse(rawData);
-                const language = jsonData?.item?.$?.language;
+  for (const item of items) {
+    const fullPath = path.join(dir, item.name);
 
-                if (language) {
-                    uniqueLanguages.add(language);
-                }
-            } catch (error) {
-                console.error(`Error reading ${fullPath}:`, error.message);
-            }
+    if (item.isDirectory()) {
+      findAndExtractLanguages(fullPath); // Proper recursion
+    } else if (item.isFile() && item.name === 'data.json.json') {
+      try {
+        const rawData = fs.readFileSync(fullPath, 'utf8');
+        const jsonData = JSON.parse(rawData);
+        const language = jsonData?.item?.$?.language;
+
+        if (language) {
+          uniqueLanguages.add(language);
         }
+      } catch (error) {
+        console.error(`Error reading ${fullPath}:`, error.message);
+      }
     }
-    return uniqueLanguages;
+  }
+  return uniqueLanguages;
 };
-
-module.exports = extractLocales;
+module.exports = findAndExtractLanguages;
