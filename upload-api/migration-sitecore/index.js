4c05370d21e348d496c3bd3960a68ea931d3dd16
AishDani <aishwarya.dani@contentstack.com>
2025-03-10 10:54:10 +0530

    refactor:conflict in upload siteocore service

--- upload-api/migration-sitecore/index.js
+++ upload-api/migration-sitecore/index.js
@@ -1,19 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const contentTypes = require("./libs/contenttypes.js")
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ExtractConfiguration = require("./libs/configuration.js")
+const contentTypes = require('./libs/contenttypes.js');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const reference = require("./libs/reference.js")
+const ExtractConfiguration = require('./libs/configuration.js');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ExtractFiles = require("./libs/convert.js")
+const reference = require('./libs/reference.js');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const extractLocales = require("./libs/extractLocales.js")
+const ExtractFiles = require('./libs/convert.js');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { findAndExtractLanguages } = require('./libs/extractLocales.js');
 
-module.exports  = {
+module.exports = {
   contentTypes,
   ExtractConfiguration,
   reference,
-  ExtractFiles, 
-  extractLocales
-}
+  ExtractFiles,
+  findAndExtractLanguages
+};
