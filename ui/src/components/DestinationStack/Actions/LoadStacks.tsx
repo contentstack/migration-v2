db13503b401c293d20d7f3bf9b9697f260e3620e
AishDani <aishwarya.dani@contentstack.com>
2025-04-21 10:11:12 +0530

    fix:removed console

--- ui/src/components/DestinationStack/Actions/LoadStacks.tsx
+++ ui/src/components/DestinationStack/Actions/LoadStacks.tsx
@@ -15,7 +15,7 @@ import { RootState } from '../../../store';
 import { updateNewMigrationData } from '../../../store/slice/migrationDataSlice';
 
 // Interface
-import { IDropDown, INewMigration } from '../../../context/app/app.interface';
+import { DEFAULT_DROPDOWN, IDropDown, INewMigration } from '../../../context/app/app.interface';
 import { StackResponse } from '../../../services/api/service.interface';
 import { Stack } from '../../../components/Common/AddStack/addStack.interface';
 
@@ -373,7 +373,7 @@ const LoadStacks = (props: LoadFileFormatProps) => {
             </Tooltip>
           </div>
           <LanguageMapper
-          stack={selectedStack} />
+          stack={selectedStack ?? DEFAULT_DROPDOWN} />
         </div>
       )}
     </div>
