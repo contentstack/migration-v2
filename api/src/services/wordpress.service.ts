583ead4a3e751802f54a73f9615443c9e772231c
RohitKini <57343193+RohitKini@users.noreply.github.com>
2025-04-24 12:17:53 +0530

    changes for testing workflow

--- api/src/services/wordpress.service.ts
+++ api/src/services/wordpress.service.ts
@@ -11,6 +11,7 @@ import { v4 as uuidv4 } from "uuid";
 import { orgService } from "./org.service.js";
 
 
+
 const { JSDOM } = jsdom;
 const virtualConsole = new jsdom.VirtualConsole();
 // Get the current file's path
