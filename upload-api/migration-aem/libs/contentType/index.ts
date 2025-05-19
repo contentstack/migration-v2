fcfd245c132022c2afd48cb2451ca4288fd7e194
umeshmore45 <umesh.more@contentstack.com>
2025-05-20 01:42:34 +0530

    Add migration-aem module with initial setup and configuration

---
+++ upload-api/migration-aem/libs/contentType/index.ts
@@ -0,0 +1,11 @@
+import { IConvertContentType } from "./types/index.interface";
+import read from 'fs-readdir-recursive';
+
+
+const convertContentType: IConvertContentType = (path) => {
+  console.info("umesbisb", path);
+}
+
+
+
+export default convertContentType;
