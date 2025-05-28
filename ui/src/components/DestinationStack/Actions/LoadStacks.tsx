4a6f9692be4fbd7438ff595503f10ac933a55f59
AishDani <aishwarya.dani@contentstack.com>
2025-05-28 15:43:12 +0530

    fix:resolved language mapper bugs and added custom starting message for migration

--- ui/src/components/DestinationStack/Actions/LoadStacks.tsx
+++ ui/src/components/DestinationStack/Actions/LoadStacks.tsx
@@ -15,7 +15,7 @@ import { RootState } from '../../../store';
 import { updateNewMigrationData } from '../../../store/slice/migrationDataSlice';
 
 // Interface
-import { DEFAULT_DROPDOWN, IDropDown, INewMigration } from '../../../context/app/app.interface';
+import { IDropDown, INewMigration } from '../../../context/app/app.interface';
 import { StackResponse } from '../../../services/api/service.interface';
 import { Stack } from '../../../components/Common/AddStack/addStack.interface';
 
@@ -176,7 +176,8 @@ const LoadStacks = (props: LoadFileFormatProps) => {
         ...newMigrationData,
         destination_stack: {
           ...newMigrationData?.destination_stack,
-          selectedStack: { ...data }
+          selectedStack: { ...data },
+          localeMapping: data?.value ? newMigrationData?.destination_stack?.localeMapping : {}
         }
       };
 
@@ -374,7 +375,7 @@ const LoadStacks = (props: LoadFileFormatProps) => {
           </div>
           <LanguageMapper
           uid={selectedStack?.uid ?? ''}
-          stack={selectedStack ?? DEFAULT_DROPDOWN} />
+          stack={selectedStack} />
         </div>
       )}
     </div>
