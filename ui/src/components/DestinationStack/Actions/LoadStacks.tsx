43f1c94fad875ba487c5f3e1b4d8c93c0ceb0a58
AishDani <aishwarya.dani@contentstack.com>
2025-05-30 15:27:38 +0530

    refactor:resolved talisman errors

--- ui/src/components/DestinationStack/Actions/LoadStacks.tsx
+++ ui/src/components/DestinationStack/Actions/LoadStacks.tsx
@@ -15,7 +15,7 @@ import { RootState } from '../../../store';
 import { updateNewMigrationData } from '../../../store/slice/migrationDataSlice';
 
 // Interface
-import { IDropDown, INewMigration } from '../../../context/app/app.interface';
+import { DEFAULT_DROPDOWN, IDropDown, INewMigration } from '../../../context/app/app.interface';
 import { StackResponse } from '../../../services/api/service.interface';
 import { Stack } from '../../../components/Common/AddStack/addStack.interface';
 
@@ -176,8 +176,7 @@ const LoadStacks = (props: LoadFileFormatProps) => {
         ...newMigrationData,
         destination_stack: {
           ...newMigrationData?.destination_stack,
-          selectedStack: { ...data },
-          localeMapping: data?.value ? newMigrationData?.destination_stack?.localeMapping : {}
+          selectedStack: { ...data }
         }
       };
 
@@ -375,7 +374,7 @@ const LoadStacks = (props: LoadFileFormatProps) => {
           </div>
           <LanguageMapper
           uid={selectedStack?.uid ?? ''}
-          stack={selectedStack} />
+          stack={selectedStack ?? DEFAULT_DROPDOWN} />
         </div>
       )}
     </div>
