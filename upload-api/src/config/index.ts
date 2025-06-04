96be27db1bf3aef925597d578be7dd7c2f2e19c0
shobhit upadhyay <shobhit.upadhyay112@gmail.com>
2025-06-04 16:57:15 +0530

    page CT added

--- upload-api/src/config/index.ts
+++ upload-api/src/config/index.ts
@@ -2,7 +2,7 @@ export default {
   plan: {
     dropdown: { optionLimit: 100 }
   },
-  cmsType: 'contentful',
+  cmsType: 'wordpress',
   isLocalPath: true,
   awsData: {
     awsRegion: 'us-east-2',
@@ -12,5 +12,5 @@ export default {
     bucketName: '',
     bucketKey: ''
   },
-  localPath: 'your-local-legacy-cms-path',
+  localPath: '/Users/shobhit.upadhyay/Downloads/blogpost.xml',
 };
