7df88548797f3399158d1206b6ae4a0f8e777151
samarp-jain <samarp.jain@contentstack.com>
2025-06-06 10:22:01 +0530

    refactor: clean up formatting and improve readability in ContentMapper and LoadUploadFile components; enhance validation logic and state management in Migration component

--- upload-api/src/config/index.ts
+++ upload-api/src/config/index.ts
@@ -2,7 +2,7 @@ export default {
   plan: {
     dropdown: { optionLimit: 100 }
   },
-  cmsType: 'wordpress',
+  cmsType: 'sitecore',
   isLocalPath: true,
   awsData: {
     awsRegion: 'us-east-2',
@@ -12,5 +12,5 @@ export default {
     bucketName: '',
     bucketKey: ''
   },
-  localPath: 'your-local-legacy-cms-path', 
+  localPath: '/Users/samarp.jain/Desktop/package 45.zip',
 };
