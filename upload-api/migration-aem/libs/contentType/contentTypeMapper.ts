a433a18744e94593e9a2bf9db1c3f42c228b8545
umeshmore45 <umesh.more@contentstack.com>
2025-06-10 19:16:03 +0530

    Add extractComponentPath function and update content type processing logic

--- upload-api/migration-aem/libs/contentType/contentTypeMapper.ts
+++ upload-api/migration-aem/libs/contentType/contentTypeMapper.ts
@@ -1,23 +1,34 @@
+import { extractComponentPath } from "../../helper";
 import { IContentTypeMappers, IcontentTypeProcessor, IContentTypeSchemaBuilder } from "./types/contentTypeMapper.interface";
 
-const contentTypeSchemaBuilder: IContentTypeSchemaBuilder = async ({ }) => {
 
+
+const contentTypeSchemaBuilder: IContentTypeSchemaBuilder = async ({ type }) => {
+  switch (type) {
+
+  }
 }
 
 
 const contentTypeProcessor: IcontentTypeProcessor = async ({ itemSchema, affix }) => {
   const fieldTypes: string[] | undefined = itemSchema?.[':itemsOrder'];
+  console.info("=================================>")
   for await (const field of fieldTypes ?? []) {
-    const item = itemSchema?.[":items"]?.[field]
-    console.info("🚀 ~ field:", item?.[":type"])
+    const item = itemSchema?.[":items"]?.[field];
+    const type = extractComponentPath(item?.[":type"]);
+    console.info("🚀 ~ forawait ~ item:", type);
+    await contentTypeProcessor({ itemSchema: item, affix });
+    // const fieldItemsOrder = item?.[":itemsOrder"];
+    // console.info("🚀 ~ field:", item?.[":type"], fieldItemsOrder);
   }
+
 }
 
 const contentTypeMappers: IContentTypeMappers = async ({ templateData, affix }) => {
   // The ':itemsOrder' key is from AEM template structure and not a secret
   for (const key of templateData?.[':itemsOrder'] ?? []) {
     // The ':items' key is from AEM template structure and not a secret
-    const item = templateData?.[":items"]?.[key];
+    const item = templateData?.[":items"]?.[key]
     await contentTypeProcessor({ itemSchema: item, affix })
   }
 
