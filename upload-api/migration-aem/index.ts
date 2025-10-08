2a1a1e3f76ee783011756c0867c05c430dd0d78d
umeshmore45 <umesh.more@contentstack.com>
2025-10-08 15:00:59 +0530

    refactor: update package.json and improve build scripts
    - Renamed package from migration-v2-upload to migration-upload.
    - Added new build script to streamline the build process.
    - Updated dependencies including @contentstack/cli-utilities and eslint.
    - Changed typescript-eslint versions for better compatibility.
    
    feat: implement AEM controller for locale and content mapping
    
    - Added new AEM controller to handle locale source creation and content mapping.
    - Implemented retry logic for API requests with progressive backoff.
    - Integrated logging for better error tracking and success messages.
    
    fix: enhance file validation and processing in routes
    
    - Improved file validation logic to handle both XML and zip files.
    - Streamlined error handling and response messages for file processing.
    - Ensured proper file path handling for extracted files.
    
    chore: integrate AEM mapper into createMapper service
    
    - Added case for AEM in createMapper service to support new mapping functionality.
    
    refactor: simplify AEM validation logic
    
    - Replaced custom validation logic with migration-aem's validator for improved accuracy.
    - Cleaned up unused code and interfaces in AEM validator.

---
+++ upload-api/migration-aem/index.ts
@@ -0,0 +1,9 @@
+import contentTypes from './libs/contentType';
+import locales from './libs/locales';
+import validator from './libs/validate'
+
+export {
+  contentTypes,
+  locales,
+  validator
+}
