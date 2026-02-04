e9445580eaf3b08016a04f4427315a21fba1a580
yashin4112 <yash.shinde@contentstack.com>
2026-02-04 15:10:00 +0530

    entry update code added, entry-mapper update status code updated

--- api/src/services/contentMapper.service.ts
+++ api/src/services/contentMapper.service.ts
@@ -26,7 +26,7 @@ import getProjectUtil from "../utils/get-project.utils.js";
 import fetchAllPaginatedData from "../utils/pagination.utils.js";
 import ProjectModelLowdb from "../models/project-lowdb.js";
 import getFieldMapperDb from "../models/FieldMapper.js";
-import getEntryMapperDb from "../models/EntryMapper.js";
+import getEntryMapperDb, { EntryMapper } from "../models/EntryMapper.js";
 import { v4 as uuidv4 } from "uuid";
 import getContentTypesMapperDb, { ContentTypesMapper } from "../models/contentTypesMapper-lowdb.js";
 import getUidMapperDb from "../models/uidMapper.js";
@@ -2094,10 +2094,25 @@ const updateEntryStatus = async (req: Request) => {
 }
 
 
-const updateEntryMapping = async (req: Request) => {
-  const { projectId, otherCmsEntryUid } = req.params;
-
+const updateEntryStatus = async (req: Request) => { 
+  const { projectId } = req.params;
+  const { otherCmsEntryUids } = req.body;
+  const validatedUids: string[] = Array.isArray(otherCmsEntryUids) ? otherCmsEntryUids : [];
   const srcFunc = "updateEntryMapping";
+  if (isEmpty(validatedUids)) {
+    logger.error(
+      getLogMessage(
+        srcFunc,
+        "Invalid otherCmsEntryUids"
+      )
+    );
+    return {
+      status: HTTP_CODES?.BAD_REQUEST,
+      data: {
+        message: "Invalid otherCmsEntryUids",
+      },
+    };  
+  }
   try {
     await ProjectModelLowdb.read();
     const projectData = ProjectModelLowdb.chain
@@ -2107,12 +2122,12 @@ const updateEntryMapping = async (req: Request) => {
     const iteration = projectData?.iteration || 1;
     const EntryMapperModel = getEntryMapperDb(projectId, iteration);
     await EntryMapperModel.read();
-    let foundEntry: any = null;
+    const foundEntry: EntryMapper[] = [];
     await EntryMapperModel.update((data: any) => {
       data?.entry_mapper?.forEach((entry: any) => {
-        if (otherCmsEntryUid === entry?.otherCmsEntryUid) {
+        if (validatedUids.includes(entry?.otherCmsEntryUid)) {
           entry.isUpdate = true;
-          foundEntry = entry;
+          foundEntry.push(entry);
         }
       });
     });
