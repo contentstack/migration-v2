9dff43545d5d1bd4297d97532d8ba1fb2e8fefcc
AishDani <aishwarya.dani@contentstack.com>
2026-02-09 15:03:13 +0530

    refactor:Refactor taxonomy handling in extractItems and extractTaxonomy; update build script in package.json for improved compatibility

--- upload-api/migration-wordpress/libs/extractTaxonomy.ts
+++ upload-api/migration-wordpress/libs/extractTaxonomy.ts
@@ -2,15 +2,28 @@ import { Categories, Field } from "../interface/interface";
 
 const handleTaxonomySchema = async(categories: any, allCategories : Categories[]) => {
 
-    const taxonomyArray: string[] = [];
+    const taxonomyArray: any[] = [];
     for(const category of categories){
         const categoryData = allCategories?.find((item: any) => item?.["wp:category_nicename"] === category?.attributes?.["nicename"]);
 
         if(categoryData  && !categoryData?.['wp:category_parent']){
-            taxonomyArray?.push(`${categoryData?.["wp:category_nicename"]}_${categoryData?.["wp:term_id"]}`)
+            taxonomyArray?.push(
+            {
+                "taxonomy_uid":  `${categoryData?.["wp:category_nicename"]}_${categoryData?.["wp:term_id"]}`,
+                "mandatory": false,
+                "multiple": true,
+                "non_localizable": false
+            }
+               
+            )
         } else if(categoryData?.['wp:category_parent']) {
             const parentCategory = allCategories?.find((category: any) => category?.["wp:category_nicename"] === categoryData?.['wp:category_parent']);
-            taxonomyArray?.push(`${parentCategory?.["wp:category_nicename"]}_${parentCategory?.["wp:term_id"]}`)
+            taxonomyArray?.push({
+                "taxonomy_uid": `${parentCategory?.["wp:category_nicename"]}_${parentCategory?.["wp:term_id"]}`,
+                "mandatory": false,
+                "multiple": true,
+                "non_localizable": false
+            })
         } 
     }
     return taxonomyArray;
