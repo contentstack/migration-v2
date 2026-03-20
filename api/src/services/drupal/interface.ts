625e3f86c35332d11bbbff5a3d1fb7e696778669
AishDani <aishwarya.dani@contentstack.com>
2026-03-20 16:24:13 +0530

    refactor: resolved copilot review

--- api/src/services/drupal/interface.ts
+++ api/src/services/drupal/interface.ts
@@ -1,12 +1,12 @@
 export interface DbConfig {
-    host: string;
-    user: string;
-    password: string;
-    database: string;
-    port: number | string;
+  host: string;
+  user: string;
+  password: string;
+  database: string;
+  port: number | string;
 }
 
 export interface AssetsConfig {
-    base_url?: string;
-    public_path?: string;
+  base_url?: string;
+  public_path?: string;
 }
