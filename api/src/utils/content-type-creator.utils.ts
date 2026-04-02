5fd69e0fedd73ffd5b4ddc93a80c428b9ecae7d3
AishDani <aishwarya.dani@contentstack.com>
2026-04-02 13:11:49 +0530

    refactor:resolved PR comments

--- api/src/utils/content-type-creator.utils.ts
+++ api/src/utils/content-type-creator.utils.ts
@@ -1026,8 +1026,27 @@ const writeGlobalField = async (schema: any, globalSave: string) => {
   }
 };
 
+const resolveIsSsoFlag = (is_sso: any): boolean => {
+  if (typeof is_sso === 'boolean') {
+    return is_sso;
+  }
+
+  if (is_sso === 'true') {
+    return true;
+  }
+
+  if (is_sso === 'false') {
+    return false;
+  }
+
+  throw new Error(
+    `Invalid token_payload.is_sso in existingCtMapper; expected boolean, received: ${JSON.stringify(is_sso)}`
+  );
+};
+
 const existingCtMapper = async ({ keyMapper, contentTypeUid, projectId, region, user_id, is_sso, type}: any) => {
   try {
+    const normalizedIsSso = resolveIsSsoFlag(is_sso);
     const ctUid = keyMapper?.[contentTypeUid];
 
     if(type === 'global_field') {
@@ -1041,7 +1060,7 @@ const existingCtMapper = async ({ keyMapper, contentTypeUid, projectId, region, 
           token_payload: {
             region,
             user_id,
-            is_sso
+            is_sso: normalizedIsSso
           }
         }
       }
@@ -1057,7 +1076,7 @@ const existingCtMapper = async ({ keyMapper, contentTypeUid, projectId, region, 
           token_payload: {
             region,
             user_id,
-            is_sso
+            is_sso: normalizedIsSso
           }
         }
       }
