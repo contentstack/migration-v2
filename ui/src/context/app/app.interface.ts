c94f85757d71e62490af8466ca061afbb5093e14
Aishwarya <aishwarya.dani@contentstack.com>
2026-04-27 11:14:07 +0530

    Revert "feat: add AutoMappedMergeConfirmModal component and enhance SaveChangesModal with async handling for step changes"
    This reverts commit fe3bfed302148f6bf9be9463442e70cebd6a9a7a.

--- ui/src/context/app/app.interface.ts
+++ ui/src/context/app/app.interface.ts
@@ -191,8 +191,8 @@ export interface IDestinationStack {
   csLocale: string[];
 }
 export interface IContentMapper {
-  existingGlobal: ContentTypeList[];
-  existingCT: ContentTypeList[];
+  existingGlobal: ContentTypeList[] | (() => ContentTypeList[]);
+  existingCT: ContentTypeList[] | (() => ContentTypeList[]);
   content_type_mapping: ContentTypeMap;
   isDropDownChanged?: boolean;
   otherCmsTitle?: string;
